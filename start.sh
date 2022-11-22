4577b86d0f11b43ca90631b47a56ca9e555da5d9
nezukotanji <118502981+nezukotanji@users.noreply.github.com>
2022-11-22 10:12:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Masterrockiei/Sakura.git /Sakura
+  git clone https://github.com/nezukotanji/tanjiro.git /tanjiro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Sakura
+  git clone $UPSTREAM_REPO /tanjiro
 fi
-cd /Sakura
+cd /tanjiro
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
