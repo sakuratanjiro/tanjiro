2f34ed5b841ada7b6c96ae83fbfc9bc8b76fed6a
Amal nath <nath38302@gmail.com>
2022-11-23 05:41:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/nezukotanji/tanjiro.git /tanjiro
+  git clone https://github.com/Masterrockiei/sakuraa /sakuraa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /tanjiro
+  git clone $UPSTREAM_REPO /sakuraa
 fi
-cd /tanjiro
+cd /sakuraa
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
